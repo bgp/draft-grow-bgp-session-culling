026b59ebb2470fec3c9660b4756067f03b4f9767
Matt Griswold <grizz@20c.com>
2017-09-27 18:39:55 +0000

    update livereload

--- serve.py
+++ serve.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
+import os
 from livereload import Server, shell
 
 
+ROOT_DIR='live/'
+if not os.path.exists(ROOT_DIR):
+    os.mkdir(ROOT_DIR)
 server = Server()
-server.watch('*.xml', shell('xml2rfc draft-iops-grow-bgp-session-culling.xml -o live/index.html --html'))
-server.serve(root='live/')
+server.watch('*.xml', shell('xml2rfc draft-ietf-grow-bgp-session-culling.xml -o live/index.html --html'))
+server.serve(root=ROOT_DIR)
